0620f5b2bee32236b773eb9f3d334cc97c17ae16
beelarr <bryonl@me.com>
2017-11-24 14:50:17 -0600

    Add inital structure and understanding JSX composistion

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, Im an react app</h1>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1'))
+  }
+}
+
+export default App;
